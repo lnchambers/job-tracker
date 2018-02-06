799f63028041e9e241722cba792bb26f62f6897c
Luke Chambers <lukenatchambers@gmail.com>
2018-02-06 13:33:53 -0700

    Make seed file work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,17 @@
 Company.destroy_all
 Job.destroy_all
+Category.destroy_all
 
 COMPANIES = ["ESPN", "Aetna", "United Airlines", "Denver Public Schools", "Shopify", "Starbucks", "Pivotal Labs", "Captain U"]
 JOBS = ["Engineering", "Development", "Dev Ops", "Quality Assurance", "Teacher", "Product Manager", "Consultant", "Community Manager"]
 CITIES = ["Seattle", "Denver", "Portland", "Indianapolis", "Madison", "Orlando", "San Diego", "Austin", "Las Vegas", "Little Rock", "Boise", "Eugene", "Oakland"]
 CATEGORIES = ["Software", "Hardware", "Middleware", "Firmware", "Warewolf", "Warebat", "Waregoing?", "Spyware", "Botware", "Wareware"]
 
-CATEGORIES.each do |title|
-  Category.create!(title: title)
-end
-
-COMPANIES.each do |name|
+COMPANIES.zip(CATEGORIES).each do |name, title|
   company = Company.create!(name: name)
-  puts "Created #{company.name}"
+  category = Category.create!(title: title)
   10.times do |num|
-    company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample, categories_id: rand(10) + 1)
-    puts "Created #{company.jobs[num].title}"
+    company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample, category: category)
+    puts "  Created #{company.jobs[num].title}"
   end
 end
