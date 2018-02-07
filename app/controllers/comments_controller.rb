50f095cbd703b9090eace0480b7604eef0c5ab09
Luke Chambers <lukenatchambers@gmail.com>
2018-02-06 21:34:16 -0700

    Create functionality complete for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new(comment_params)
-    @comment.job_id = params[job_id]
+    @comment.job_id = params[:job_id]
     if @comment.save
       flash[:success] = "Comment added!"
       redirect_to job_path(@comment.job)
