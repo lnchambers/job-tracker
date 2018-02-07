52b0aeddfbcfa832c007b85723ffc4b9e9332f47
Luke Chambers <lukenatchambers@gmail.com>
2018-02-06 22:55:15 -0700

    Build out tests for contacts

--- spec/features/companies/user_sees_one_company_spec.rb
+++ spec/features/companies/user_sees_one_company_spec.rb
@@ -1,13 +1,31 @@
 require 'rails_helper'
 
 describe "User sees one company" do
+  before :each do
+    @company = create(:company)
+    @job = create(:job, company: @company)
+    @contact = create(:contact, company: @company)
+  end
   scenario "a user sees a company" do
-    job = create(:job)
+    visit company_path(@job.company_id)
 
-    visit company_path(job.company_id)
-
-    expect(current_path).to eq("/companies/#{job.company_id}/jobs")
+    expect(current_path).to eq("/companies/#{@job.company_id}/jobs")
     expect(page).to have_content("Opakawagalaga")
-    expect(page).to have_content("Work For Free")
+  end
+
+  scenario "a user sees a company's contacts" do
+    visit company_path(@job.company_id)
+
+    expect(page).to have_content(@company.name)
+    expect(page).to have_content(@contact.name)
+    expect(page).to have_content(@contact.title)
+    expect(page).to have_content(@contact.email)
+  end
+
+  scenario "a user sees a company's jobs" do
+    visit company_path(@job.company_id)
+
+    expect(page).to have_content(@company.name)
+    expect(page).to have_content(@job.title)
   end
 end
