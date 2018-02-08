52b8e4ee55f6bc42c30de4c6c75e9f9a46dd3eb3
Luke Chambers <lukenatchambers@gmail.com>
2018-02-07 21:50:23 -0700

    User can edit a tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -13,11 +13,26 @@ class TagsController < ApplicationController
   end
 
   def edit
+    @tag = Tag.find(params[:id])
   end
 
   def update
+    @tag = Tag.find(params[:id])
+    @tag.update(tag_params)
+    if @tag.save
+      flash[:success] = "#{@tag.title} updated!"
+      redirect_to tag_path(@tag)
+    else
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:title)
   end
 end
